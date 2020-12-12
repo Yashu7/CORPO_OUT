351df1b004cbbdff631c114969b8b96b1a77b1df
Yashu7 <janmalawski2@gmail.com>
2020-12-12 20:06:36 +0100

    Photon setup, UI setup

---
+++ Assets/Scripts/EnterPlayerName.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+[RequireComponent(typeof(InputField))]
+public class EnterPlayerName : MonoBehaviour
+{
+    //Key for fiding players name in Unity's PlayerPrefs
+    const string playerNameKey = "PlayerName";
+
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField input = this.GetComponent<InputField>();
+        if(input != null)
+        {
+            if(PlayerPrefs.HasKey(playerNameKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNameKey);
+                input.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetPlayerGlobalName(string playerName)
+    {
+        if(string.IsNullOrEmpty(playerName))
+        {
+            Debug.LogError("Player's name is empty");
+            return;
+        }
+        PhotonNetwork.NickName = playerName;
+        PlayerPrefs.SetString(playerNameKey, playerName);
+    }
+}
---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    string gameVersion = "0.0.1";
+    
+    [Tooltip("Name of created room")]
+    [SerializeField]
+    private string roomName;
+
+    void Awake()
+    {
+        //Makes sure all players are in the same room.
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    
+    void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(roomName,roomOptions,TypedLobby.Default);
+       
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room Created");
+
+    }
+    public override void OnCreateRoomFailed(short returnCode,string message)
+    {
+        Debug.Log(message);
+    }
+
+}
